4e9f16fc5b870013de034668117f04776d4700df
Eddie Gurnee <pegurnee@gmail.com>
2015-06-04 18:51:51 -0400

    set code for roulette

--- roll_a_ball/Assets/_scripts/rotator.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class rotator : MonoBehaviour {
-
-	void Update () {
-		this.transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
-	}
-}
